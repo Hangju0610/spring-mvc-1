638788371c29659ac67dfb860c1ceee32ab93c62
Hangju0610 <131460159+Hangju0610@users.noreply.github.com>
2026-04-14 17:09:13 +0900

    feat: add MVC pattern

--- src/main/java/hello/servlet/web/servlet/MemberFormServlet.java
+++ src/main/java/hello/servlet/web/servlet/MemberFormServlet.java
@@ -1,6 +1,5 @@
 package hello.servlet.web.servlet;
 
-import hello.servlet.domain.member.MemberRepository;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,8 +9,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "memberFromServlet", urlPatterns = "/servlet/members/new-form")
-public class MemberFromServlet extends HttpServlet {
+@WebServlet(name = "memberFormServlet", urlPatterns = "/servlet/members/new-form")
+public class MemberFormServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
